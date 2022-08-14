22d6097d846fd04741fac764353b076736d646f9
Сергей <mak.sergei@ngs.ru>
2022-08-14 18:21:48 +0700

    Commit HomeWork-Cycles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+
+    //task01//
+
+     int i=0;
+     while (i<10 ){
+         i++;
+         System.out.print(i);}
+        System.out.println();
+     for (i=10; i>0; i-=1){
+
+         System.out.print(i);}
+
+     //task//
+
+     int oneFreeDay=5; //первая пятница//
+     do {
+         System.out.println();
+         System.out.println("Сегодня пятница, "+oneFreeDay+ " -е число. Необходимо подготовить отчет.");
+         oneFreeDay+=7;
+     }
+     while (oneFreeDay<31);
+
+     //task//
+
+     int year200=1822;
+     for (int b=0; b<2100; b+=79){
+         if (b>=year200) System.out.println(b);}
+
+
+
+
+
+
+
+
+
+
+         }
+
+
+
 }
+
+
+
+
+
+
